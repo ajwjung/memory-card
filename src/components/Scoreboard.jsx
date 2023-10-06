121ed5ce374ac3d09137accd3bd0951efd6e9328
Amy Jung <ajwjung@outlook.com>
2023-10-06 14:36:47 -0700

    style: Heading and scoreboard

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,8 +1,12 @@
 function Scoreboard({ currentScore, highestScore }) {
     return (
         <div className="scoreboard">
-            <div className="currentScore">Current score: {currentScore}</div>
-            <div className="highestScore">Highest score: {highestScore}</div>
+            <div className="currentScore">
+                <b>Current score:</b> {currentScore}
+            </div>
+            <div className="highestScore">
+                <b>Highest score:</b> {highestScore}
+            </div>
         </div>
     )
 }
